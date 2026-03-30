8cb019c15a38f90bb11a619333eb6388e0a70077
yelasri <elasriyoussef604@gmail.com>
2026-03-30 21:47:56 +0100

    implement Security filter chain

---
+++ src/main/java/lets_play/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package lets_play.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/lets_play/user/UserController.java
+++ src/main/java/lets_play/user/UserController.java
@@ -2,9 +2,15 @@ package lets_play.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public String get() {
+        return "Hello world!";
+    }
 
 }
